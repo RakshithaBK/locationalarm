c54a53ea154c4cfd772a332ea49f681c2145df40
Niveditha.Kabbur <niveditha_kabbur@yahoo.com>
2017-01-19 12:28:45 +0530

    Merged login and Location reminder

--- app/src/main/java/com/trianz/locationalarm/LoginActivity.java
+++ app/src/main/java/com/trianz/locationalarm/LoginActivity.java
@@ -91,70 +91,75 @@ public class LoginActivity extends Fragment {
     }
 
     public void homeActivity(View view){
-        final String mobile = loginEditTextMobile.getText().toString().trim();
-        final String password = loginEditTextPassword.getText().toString().trim();
 
-        HashMap<String, String> params = new HashMap<String, String>();
-        params.put(KEY_MOBILE, mobile);
-        params.put(KEY_PASSWORD,password);
-
-        JSONObject jsonBody = new JSONObject(params);
-
-        JsonObjectRequest JsonObjRequest = new JsonObjectRequest(Request.Method.POST, REGISTER_URL ,jsonBody,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            // Parsing json object response
-                            // response will be a json object
-                            JSONObject json = new JSONObject(response.toString());
-                            Log.v("Json obj" ,json.toString());
-                            Boolean status = Boolean.parseBoolean(json.getString("status"));
-                            String message = json.getString("message");
-                            if(status==true){
-                                Toast.makeText(getActivity(),message, Toast.LENGTH_SHORT).show();
-                                Intent homeActivity = new Intent(getContext(),HomeActivity.class);
+        Intent homeActivity = new Intent(getContext(),MainActivity.class);
                                 startActivity(homeActivity);
-                            }else{
-                                Toast.makeText(getActivity(),message, Toast.LENGTH_SHORT).show();
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        String message = null;
-                        if (volleyError instanceof NetworkError) {
-                            message = "Cannot connect to Internet...Please check your connection!";
-                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
-                        } else if (volleyError instanceof ServerError) {
-                            message = "The server could not be found. Please try again after some time!!";
-                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
-                        } else if (volleyError instanceof AuthFailureError) {
-                            message = "Cannot connect to Internet...Please check your connection!";
-                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
-                        } else if (volleyError instanceof ParseError) {
-                            message = "Parsing error! Please try again after some time!!";
-                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
-                        } else if (volleyError instanceof NoConnectionError) {
-                            message = "Cannot connect to Internet...Please check your connection!";
-                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
-                        } else if (volleyError instanceof TimeoutError) {
-                            message = "Connection TimeOut! Please check your internet connection.";
-                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
-                        }
-                    }
 
 
-                });
-
-        MySingleton.getInstance(getActivity()).addToRequestQueue(JsonObjRequest);
-//        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
-//        requestQueue.add(JsonObjRequest );
+//        final String mobile = loginEditTextMobile.getText().toString().trim();
+//        final String password = loginEditTextPassword.getText().toString().trim();
+//
+//        HashMap<String, String> params = new HashMap<String, String>();
+//        params.put(KEY_MOBILE, mobile);
+//        params.put(KEY_PASSWORD,password);
+//
+//        JSONObject jsonBody = new JSONObject(params);
+//
+//        JsonObjectRequest JsonObjRequest = new JsonObjectRequest(Request.Method.POST, REGISTER_URL ,jsonBody,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        try {
+//                            // Parsing json object response
+//                            // response will be a json object
+//                            JSONObject json = new JSONObject(response.toString());
+//                            Log.v("Json obj" ,json.toString());
+//                            Boolean status = Boolean.parseBoolean(json.getString("status"));
+//                            String message = json.getString("message");
+//                            if(status==true){
+//                                Toast.makeText(getActivity(),message, Toast.LENGTH_SHORT).show();
+//                                Intent homeActivity = new Intent(getContext(),HomeActivity.class);
+//                                startActivity(homeActivity);
+//                            }else{
+//                                Toast.makeText(getActivity(),message, Toast.LENGTH_SHORT).show();
+//                            }
+//
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError volleyError) {
+//                        String message = null;
+//                        if (volleyError instanceof NetworkError) {
+//                            message = "Cannot connect to Internet...Please check your connection!";
+//                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+//                        } else if (volleyError instanceof ServerError) {
+//                            message = "The server could not be found. Please try again after some time!!";
+//                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+//                        } else if (volleyError instanceof AuthFailureError) {
+//                            message = "Cannot connect to Internet...Please check your connection!";
+//                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+//                        } else if (volleyError instanceof ParseError) {
+//                            message = "Parsing error! Please try again after some time!!";
+//                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+//                        } else if (volleyError instanceof NoConnectionError) {
+//                            message = "Cannot connect to Internet...Please check your connection!";
+//                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+//                        } else if (volleyError instanceof TimeoutError) {
+//                            message = "Connection TimeOut! Please check your internet connection.";
+//                            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//
+//
+//                });
+//
+//        MySingleton.getInstance(getActivity()).addToRequestQueue(JsonObjRequest);
+        //RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        //requestQueue.add(JsonObjRequest );
 
     }
 }
--- app/src/main/java/com/trianz/locationalarm/MainActivity.java
+++ app/src/main/java/com/trianz/locationalarm/MainActivity.java
@@ -1,14 +1,474 @@
 package com.trianz.locationalarm;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.Settings;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.trianz.locationalarm.Utils.GeofenceController;
+import com.trianz.locationalarm.Utils.NamedGeofence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+    LocationRequest mLocationRequest;
+    BottomSheetBehavior mBottomSheetBehavior1;
+    private String TAG = "Location Alarm";
+    Place selectedPlace = null;
+    RecyclerView recyclerView;
+    private RemindersListAdapter remindersListAdapter;
+    static final int SET_REMINDER_REQUEST = 1; // The request code
+    TextView reminderError;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View bottomSheet = findViewById(R.id.bottom_sheet1);
+        mBottomSheetBehavior1 = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehavior1.setHideable(false);
+        mBottomSheetBehavior1.setPeekHeight(400);
+
+        reminderError = (TextView) findViewById(R.id.reminder_error_msg);
+
+        if(!isNetworkAvailable()) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.turn_on_internet)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            MainActivity.this.finish();
+                        }
+                    }).setCancelable(false)
+                    .create()
+                    .show();
+        }
+
+
+        if(!isLocationEnabled(this)) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.turn_on_gps)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            MainActivity.this.finish();
+                        }
+                    }).setCancelable(false)
+                    .create()
+                    .show();
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(selectedPlace == null)
+                {
+                    Snackbar.make(view, "Select a reminder location.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+                else {
+                    Intent addReminderActivity = new Intent(MainActivity.this, AddReminderActivity.class);
+                    addReminderActivity.putExtra("reminder_place", (Parcelable) selectedPlace);
+                    startActivityForResult(addReminderActivity, SET_REMINDER_REQUEST);
+                }
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+
+                Log.i(TAG, "Place: " + place.getName());
+                selectedPlace = place;
+
+                // Add a marker to the selected place
+                LatLng location = place.getLatLng();
+                mMap.clear();
+                mMap.addMarker(new MarkerOptions().position(location).title(place.getName().toString()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+                try {
+
+                    getSupportActionBar().setTitle(selectedPlace.getName());
+                    getSupportActionBar().setSubtitle(selectedPlace.getAddress());
+
+                } catch (NullPointerException e)
+                {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+
+        GeofenceController.getInstance().init(this);
+
+        recyclerViewSetter();
     }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+      /*  List<NamedGeofence> remindersOnMap = new ArrayList<>();
+        remindersOnMap = GeofenceController.getInstance().getNamedGeofences();
+
+        for(int i=0; i< remindersOnMap.size(); i++)
+        {
+            double latitude = remindersOnMap.get(i).latitude;
+            double longitude = remindersOnMap.get(i).longitude;
+            String reminder = remindersOnMap.get(i).reminder_msg;
+            mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude))
+                    .title(reminder).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+
+        }  */
+
+
+        //Initialize Google Play Services
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+        else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mLastLocation = location;
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker.remove();
+        }
+
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        //move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    public boolean checkLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted. Do the
+                    // contacts-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    }
+
+                } else {
+
+                    // Permission denied, Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int googlePlayServicesCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        Log.i(MainActivity.class.getSimpleName(), "googlePlayServicesCode = " + googlePlayServicesCode);
+
+        if (googlePlayServicesCode == 1 || googlePlayServicesCode == 2 || googlePlayServicesCode == 3) {
+            GooglePlayServicesUtil.getErrorDialog(googlePlayServicesCode, this, 0).show();
+        }
+    }
+
+    public void recyclerViewSetter()
+    {
+        recyclerView = (RecyclerView) findViewById(R.id.reminders_recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+        if(GeofenceController.getInstance().getNamedGeofences().isEmpty())
+        {
+              reminderError.setVisibility(View.VISIBLE);
+        }
+        else {
+
+            reminderError.setVisibility(View.GONE);
+            remindersListAdapter = new RemindersListAdapter(GeofenceController.getInstance().getNamedGeofences());
+
+            remindersListAdapter.setListener(new RemindersListAdapter.AllGeofencesAdapterListener() {
+                @Override
+                public void onDeleteTapped(NamedGeofence namedGeofence) {
+                    List<NamedGeofence> namedGeofences = new ArrayList<>();
+                    namedGeofences.add(namedGeofence);
+                    GeofenceController.getInstance().removeGeofences(namedGeofences, geofenceControllerListener);
+                }
+            });
+
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setAdapter(remindersListAdapter);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SET_REMINDER_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                refresh();
+
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+
+                // Do nothing
+            }
+        }
+    }
+
+    // region GeofenceControllerListener
+
+    private GeofenceController.GeofenceControllerListener geofenceControllerListener = new GeofenceController.GeofenceControllerListener() {
+        @Override
+        public void onGeofencesUpdated() {
+
+           refresh();
+        }
+
+        @Override
+        public void onError() {
+
+           showErrorToast();
+        }
+    };
+
+    private void refresh() {
+
+        if(GeofenceController.getInstance().getNamedGeofences().isEmpty())
+        {
+            reminderError.setVisibility(View.VISIBLE);
+        }
+        else if(GeofenceController.getInstance().getNamedGeofences().size() == 1)
+        {
+           recyclerViewSetter();
+        }
+      else {
+
+            remindersListAdapter.notifyDataSetChanged();
+        }
+
+    }
+
+    private void showErrorToast() {
+        Toast.makeText(MainActivity.this, MainActivity.this.getString(R.string.Toast_error), Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        }else{
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+
+    }
+
 
 }
